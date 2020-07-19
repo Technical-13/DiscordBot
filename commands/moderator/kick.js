78d9f83d7c815c7f4a8826dd11ad5995a4b28199
Donald J. Fortier II <Technical_13@yahoo.com>
2020-07-19 11:09:42 -0400

    Add files via upload
    From LOTRObot -- has some server specific stuff I need to clean up at some point...

--- commands/moderator/kick.js
+++ commands/moderator/kick.js
@@ -1,3 +1,4 @@
+const bot = 'LOTRObot';
 const Discord = require( 'discord.js' );
 const commando = require( 'discord.js-commando' );
 const path = require( 'path' );
@@ -6,7 +7,6 @@ const objTimeString = {
   hour: 'numeric', minute: 'numeric', second: 'numeric',
   timeZone: 'America/New_York', timeZoneName: 'short' };
 var settings = require( path.join( __dirname, '../../../settings.json' ) );
-const bot = 'LOTRObot';
 
 class KickUser extends commando.Command {
   constructor( client ) {
@@ -22,13 +22,10 @@ class KickUser extends commando.Command {
   async run( message, strArgs ) {if(message.guild.id==='201024322444197888'){
     var isOwner = await ( settings[ bot ].owners.indexOf( message.author.id ) !== -1 ? true : false );
     var isBotMod = await ( settings[ bot ].moderators.indexOf( message.author.id ) !== -1 ? true : false );
-    var objAdminRoles = [];
     var isCrown = false;
-    var isAdmin = false;
-    var objModRole = false;
-    var isMod = false;
-    var objStaffRole = false;
-    var isStaff = false;
+    var objAdminRoles = [], isAdmin = false;
+    var objModRole = false, isMod = false;
+    var objStaffRole = false, isStaff = false;
     var strStaffChanID = '201689362906218497';
     if ( message.guild ) {
       isCrown = ( message.author.id === message.guild.owner.user.id ? true : false );
@@ -37,9 +34,7 @@ class KickUser extends commando.Command {
       objStaffRole = await message.guild.roles.get( '201710935788748800' );
       isStaff = await ( objStaffRole && ( objStaffRole.members.keyArray() ).indexOf( message.author.id ) !== -1 ? true : false );
       message.guild.roles.array().forEach( function( role, index ) {
-        if ( ( new Discord.Permissions( role.permissions ) ).has( 8 ) ) {
-          objAdminRoles.push( role );
-        }
+        if ( ( new Discord.Permissions( role.permissions ) ).has( 8 ) ) { objAdminRoles.push( role ); }
       } );
       objAdminRoles.forEach( function( role, index ) {
         if ( ( role.members.keyArray() ).indexOf( message.author.id ) !== -1 || message.author.id === message.guild.ownerID ) {
@@ -48,7 +43,7 @@ class KickUser extends commando.Command {
       } );
     }
     
-    if ( isOwner || isBotMod || isCrown || isAdmin || isMod ) {
+    if ( isOwner || isBotMod || isCrown || isAdmin || isMod || isStaff ) {
       var arrUnkickable = [];
       var arrArgs = strArgs.split( ' ' );
       var arrKickList = strArgs.match( /<@!?(\d*)>/g );
@@ -62,28 +57,26 @@ class KickUser extends commando.Command {
             var objMember = message.guild.members.get( intMemberID );
             var boolMemberMod = await ( objModRole && ( objModRole.members.keyArray() ).indexOf( intMemberID ) !== -1 ? true : false );
             var boolMemberStaff = await ( objStaffRole && ( objStaffRole.members.keyArray() ).indexOf( intMemberID ) !== -1 ? true : false );
+            var objSixtyRole = await message.guild.roles.get( '453212243094536206' );
+            var boolMemberSixty = await ( objSixtyRole && ( objSixtyRole.members.keyArray() ).indexOf( intMemberID ) !== -1 ? true : false );
             var boolKickable = objMember.kickable;
-            if ( boolMemberMod || boolMemberStaff ) {
-              boolKickable = false;
-            }
+            if ( boolMemberMod || boolMemberStaff ) { boolKickable = false; }
+            if ( !( isOwner || isBotMod || isCrown || isAdmin || isMod ) && !boolMemberSixty ) { boolKickable = false; }
             if ( boolKickable ) {
-              objMember
-                .kick( strKickReason )
+              objMember.kick( strKickReason )
                 .catch( kickError => {
                   arrUnkickable.push( intMemberID );
                   console.error( 'Encountered an error attempting to kick ' + objMember.user.tag + ' from ' + message.guild.name + '#' + message.guild.id + ': ' + kickError );
                 } );
-            } else {
-              arrUnkickable.push( intMemberID );
             }
+            else { arrUnkickable.push( intMemberID ); }
           } );
           if ( arrUnkickable.length !== 0 ) {
             var arrUnkicked = [];
             arrUnkickable.forEach( function( member ) {
-              if ( arrUnkicked.indexOf( member ) === -1 ) {
-                arrUnkicked.push( member );
-              }
-            } );console.log( 'arrUnkicked has ' + arrUnkicked.length + ' unique members.' );
+              if ( arrUnkicked.indexOf( member ) === -1 ) { arrUnkicked.push( member ); }
+            } );
+            console.log( 'arrUnkicked has ' + arrUnkicked.length + ' unique members.' );
             message.reply( 'sorry, I was unable to kick: <@' + arrUnkicked.join( '> - <@' ) + '>' );
           }
         } else {

1533d0fc09c612585e1bc1679b4187ad428200c8
Donald J. Fortier II <Technical_13@yahoo.com>
2021-03-30 13:05:48 -0400

    Sync with repl.it version.

---
+++ commands/util/dver.js
@@ -0,0 +1,28 @@
+const Discord = require( 'discord.js' );
+const commando = require( 'discord.js-commando' );
+const objTimeString = {
+  year: 'numeric', month: 'long', day: 'numeric',
+  hour: 'numeric', minute: 'numeric', second: 'numeric',
+  timeZone: 'America/New_York', timeZoneName: 'short' };
+var strNow = ( new Date() ).toLocaleDateString( 'en-US', objTimeString );
+
+class Dver extends commando.Command {
+  constructor( client ) {
+    super( client, {
+      name: 'dver',
+      group: 'util',
+      memberName: 'dver',
+      aliases: [ 'dv' ],
+      description: 'Get the current bot versions.'
+    } );
+  }
+  
+  async run( message, args ) {  
+    if ( message.client.owners.map( owner => { return owner.id; } ).indexOf( message.author.id ) !== -1 ) {
+      message.reply( 'I\'m running Discord.js v'+ Discord.version + ' → <https://discord.js.org/#/docs/main/' + Discord.version + '/general/welcome>\n\twith -commando v'+ commando.version + ' → <https://discord.js.org/#/docs/commando/v' + commando.version + '/general/welcome>' );
+      message.delete().catch( errDel => { console.error( '%o: Unable to delete !%s request by %s: %o', strNow(), command, message.author.tag, errDel ); } );
+    }
+  }  
+}
+
+module.exports = Dver;

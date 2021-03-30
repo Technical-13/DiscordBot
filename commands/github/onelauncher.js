891b6ef2daa1409ca39458938210bafcea8db45d
Donald J. Fortier II <Technical_13@yahoo.com>
2021-03-30 12:36:44 -0400

    Sync with repl.it version.

--- commands/github/onelauncher.js
+++ commands/github/onelauncher.js
@@ -1,16 +1,4 @@
-const bot = 'LOTRObot';
-const Discord = require( 'discord.js' );
 const commando = require( 'discord.js-commando' );
-const path = require( 'path' );
-const fsSettings = 'settings.json';
-const settings = require( path.join( __dirname, '../../../' + fsSettings ) );
-var strSettings = JSON.stringify( settings );
-const objTimeString = {
-  year: 'numeric', month: 'long', day: 'numeric',
-  hour: 'numeric', minute: 'numeric', second: 'numeric',
-  timeZone: 'America/New_York', timeZoneName: 'short' };
-var strNow = function () { return ( new Date() ).toLocaleDateString( 'en-us', objTimeString ) };
-const isDebug = true;//settings[ bot ].onError.isDebugMode;
 
 class OneLauncher extends commando.Command {
   constructor( client ) {
@@ -23,6 +11,8 @@ class OneLauncher extends commando.Command {
   }
   
   async run( message, args ) {
+    message.delete().catch( errDel => { console.error( 'Unable to delete onelauncher request: %o', errDel ); } );
+    message.channel.send( 'Check out <@586647088268443697>\'s OneLauncher: <https://github.com/JeremyStepp/OneLauncher>' );
   }
 }
 

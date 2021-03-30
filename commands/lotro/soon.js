3b66e7f48f22dbadc63f7b6714da6e98273a9c76
Donald J. Fortier II <Technical_13@yahoo.com>
2021-03-30 12:35:51 -0400

    Sync with repl.it version.

--- commands/lotro/soon.js
+++ commands/lotro/soon.js
@@ -1,28 +1,21 @@
-const bot = 'LOTRObot';
-const Discord = require( 'discord.js' );
 const commando = require( 'discord.js-commando' );
 const path = require( 'path' );
-const settings = require( path.join( __dirname, '../../../settings.json' ) );
-const objTimeString = { timeZone: 'America/New_York',
-  year: 'numeric', month: 'long', day: 'numeric',
-  hour: 'numeric', minute: 'numeric', second: 'numeric' };
-var strNow = ( new Date() ).toLocaleDateString( 'en-US', objTimeString );
-const strScreenShotPath = path.join( __dirname, '../../images-' + bot.toLowerCase().replace( 'bot', '' ) + '/' );
-const isDebug = settings[ bot ].onError.isDebugMode;
+const bot = 'lotro';
+const strScreenShotPath = path.join( __dirname, '../../images-' + bot + '/' );
 
 class Soon extends commando.Command {
   constructor( client ) {
     super( client, {
       name: 'soon',
-      group: bot.toLowerCase().replace( 'bot', '' ),
+      group: bot,
       memberName: 'soon',
-      description: 'Soon?'
+      description: 'Soon? SOON:tm:'
     } );
   }
 
   async run( message, args ) {
     message.delete();
-    message.channel.send( { files: [ { attachment: strScreenShotPath + bot.replace( 'bot', '' ) + '-SOON.png', name: 'SOON.png' } ] } )
+    message.channel.send( { files: [ { attachment: strScreenShotPath + bot.toUpperCase() + '-SOON.png', name: 'SOON.png' } ] } )
 	}
 }
 

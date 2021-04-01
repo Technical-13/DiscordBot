334a6efea56d6aea41fdbdb42089789255c63106
Donald J. Fortier II <Technical_13@yahoo.com>
2021-04-01 14:29:43 -0400

    registerEvents()

--- LOTRObot.js
+++ LOTRObot.js
@@ -1,5 +1,6 @@
 const Discord = require( 'discord.js' );
 const Commando = require( 'discord.js-commando' );
+const fs = require( 'fs' );
 const path = require( 'path' );
 const sqlite = require( 'sqlite' );
 const sqlite3 = require( 'sqlite3' );
@@ -8,6 +9,7 @@ const keepAlive = require( './server' );
 const client = new Commando.Client( {
   commandPrefix: '~',
   owner: '440752068509040660',
+//  owner: [ '440752068509040660', '313034997193900034' ],
   invite: '<https://discord.me/TheShoeStore>',
   unknownCommandResponse: false
 } );
@@ -38,6 +40,18 @@ client.setProvider(
 keepAlive();
 client.login( process.env.TOKEN );// Don't forget to change to greenie's token when done.
 
-client.on( 'ready', () => {
-  console.log( `Connected as ${client.user.id}: ${client.user.tag}.` );
-} );
+( async function registerEvents( dir = 'events' ) {
+  let files = await fs.readdir( path.join( __dirname, dir ) );
+  for ( let file of files ) {
+    let stat = await fs.lstat( path.join( __dirname, dir, file ) );
+    if ( stat.isDirectory() ) { registerEvents( path.join( dir, file ) ); } else {
+      if ( file.endsWith( '.js' ) ) {
+        let eventName = file.substring( 0, file.indexOc( '.js' ) );
+        try {
+          let eventModule = require( path.join( __dirname, dir, file ) );
+          client.on( eventName, eventModule.bind( null, client ) );
+        } catch( errRegEvent ) { console.error( 'Error occured attempting to register event: %o', errRegEvent ); }
+      }
+    }
+  }
+} )();
